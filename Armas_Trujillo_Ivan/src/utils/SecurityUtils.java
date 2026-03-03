3e67cc2302933cf41d85b1b10eab1961494a2c02
Casta2007-ccs <ivanarmas2007@hotmail.com>
2026-03-03 12:49:18 +0000

    creacion de el intento de la encriptacion de la contraseña

--- Armas_Trujillo_Ivan/src/utils/SecurityUtils.java
+++ Armas_Trujillo_Ivan/src/utils/SecurityUtils.java
@@ -1,8 +1,28 @@
 package utils;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class SecurityUtils {
 
+    public static String hashsPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder(2 * encodedhash.length);
+            for (byte b : encodedhash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+
+                }
+                hexString.append(hex);
+
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al encriptar", e);
+        }
+
+    }
 }
