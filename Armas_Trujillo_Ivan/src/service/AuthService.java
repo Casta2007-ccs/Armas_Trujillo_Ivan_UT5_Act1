e38e53f1acdaace59046779640bc4e45146d144c
Casta2007-ccs <ivanarmas2007@hotmail.com>
2026-03-04 12:18:32 +0000

    inicio de la crecion de los servicios de autentificacion

--- Armas_Trujillo_Ivan/src/service/AuthService.java
+++ Armas_Trujillo_Ivan/src/service/AuthService.java
@@ -7,5 +7,31 @@ import java.nio.file.*;
 import java.io.IOException;
 
 public class AuthService {
+    private final Path carpetaData = Path.of("data");
+    private final Path ficheroUsers = carpetaData.resolve("user.txt");
+
+    public AuthenService(){
+        inicializarSistema();
+    }
 
 }
+
+private void inicializarSistema(){
+try {
+    if(!Files.exists(carpetaData)){
+Files.createDirectories(carpetaData);     
+    }
+    if (!Files.exists(ficheroUsers)){
+        Files.createFile(ficheroUsers);
+    }
+} catch (IOException e) {
+    System.out.println("Error crítico inicializando ficheros: " + e.getMessage());
+}
+}
+
+public boolean registrarUsuario(String email, String password) {
+        if (email.isBlank() || password.isBlank()) return false;
+        if (usuarioExiste(email)) {
+            System.out.println("Error: El usuario ya está registrado.");
+            return false;
+        }
